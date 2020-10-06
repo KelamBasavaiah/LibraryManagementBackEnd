ab482631e62e5c296c6ad786dacdc0d1ac8c30fd
SMarimuthu13 <57354218+SMarimuthu13@users.noreply.github.com>
2020-10-06 11:58:21 +0530

    Database Update

---
+++ LibraryManagement/dbo/Tables/Books.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Books] (
+    [Id]            NVARCHAR (50)  NOT NULL,
+    [BookName]      NVARCHAR (MAX) NULL,
+    [AuthorName]    NVARCHAR (MAX) NULL,
+    [Contact]       INT            NULL,
+    [Price]         MONEY          NULL,
+    [PublishedDate] DATE           NULL,
+    [Edition]       INT            NULL,
+    [Genres]        NVARCHAR (50)  NULL,
+    [Publisher]     NVARCHAR (MAX) NULL,
+    [Copies]        INT            NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
