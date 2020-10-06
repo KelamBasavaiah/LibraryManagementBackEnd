35f63dfcadb527780d889ecadbea241cdb080dfd
janashika <57354275+janashika@users.noreply.github.com>
2020-10-06 13:39:16 +0530

    add entity

---
+++ LibraryManagement.Domain/Entities/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Domain.Entities
+{
+    public class Book
+    {
+        int id, edition, price, copies, contactNo;
+        string name, authorName, publisher, genres;
+        DateTime publishedDate;
+
+        public int Id { get => id; set => id = value; }
+        public int Edition { get => edition; set => edition = value; }
+        public int Price { get => price; set => price = value; }
+        public int Copies { get => copies; set => copies = value; }
+        public int ContactNo { get => contactNo; set => contactNo = value; }
+        public string Name { get => name; set => name = value; }
+        public string AuthorName { get => authorName; set => authorName = value; }
+        public string Publisher { get => publisher; set => publisher = value; }
+        public string Genres { get => genres; set => genres = value; }
+        public DateTime PublishedDate { get => publishedDate; set => publishedDate = value; }
+
+    }
+}
