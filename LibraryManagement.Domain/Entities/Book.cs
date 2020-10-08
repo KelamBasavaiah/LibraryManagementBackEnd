e49160bd74bfdc52032be409984373b03d1618a1
basivii <kelam.basavaiah@kanini.com>
2020-10-08 14:37:29 +0530

    Entity fix for ui

--- LibraryManagement.Domain/Entities/Book.cs
+++ LibraryManagement.Domain/Entities/Book.cs
@@ -9,21 +9,21 @@ namespace LibraryManagement.Domain.Entities
     public class Book
     {
 
-        int edition, price, copies;long contactNo;
+        int Edition, Price, Copies;long contactNo;
 
-        string id,name, authorName, publisher, genres;
-        DateTime publishedDate;
+        string Id, Name, authorName, Publisher, Genres;
+        DateTime PublishedDate;
 
-        public string Id { get => id; set => id = value; }
-        public int Edition { get => edition; set => edition = value; }
-        public int Price { get => price; set => price = value; }
-        public int Copies { get => copies; set => copies = value; }
-        public long ContactNo { get => contactNo; set => contactNo = value; }
-        public string Name { get => name; set => name = value; }
-        public string AuthorName { get => authorName; set => authorName = value; }
-        public string Publisher { get => publisher; set => publisher = value; }
-        public string Genres { get => genres; set => genres = value; }
-        public DateTime PublishedDate { get => publishedDate; set => publishedDate = value; }
+        public string id { get => Id; set => Id = value; }
+        public int edition { get => Edition; set => Edition = value; }
+        public int price { get => Price; set => Price = value; }
+        public int copies { get => Copies; set => Copies = value; }
+        public long contact { get => contactNo; set => contactNo = value; }
+        public string name { get => Name; set => Name = value; }
+        public string author_name { get => authorName; set => authorName = value; }
+        public string publisher { get => Publisher; set => Publisher = value; }
+        public string genres { get => Genres; set => Genres = value; }
+        public DateTime publishedDate { get => PublishedDate; set => PublishedDate = value; }
 
     }
 }
