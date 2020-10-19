8d9d0ed64665e5df688d9012c1a9cb3e1f88e3d9
basivii <kelam.basavaiah@kanini.com>
2020-10-19 22:42:19 +0530

    User

--- LibraryManagement.Domain/LMBL/UserBL.cs
+++ LibraryManagement.Domain/LMBL/UserBL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManagement.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace LibraryManagement.Domain.LMBL
 {
-    class UserBL
+    public class UserBl : IUserBl
     {
+        IUserRepository userObj;
+        public UserBl(IUserRepository userObj)
+        {
+            this.userObj = userObj;
+        }
+        public bool getUser(string username, string password)
+        {
+            bool result = false;
+            string dbPasssword = userObj.getUser(username);
+            if(dbPasssword== password) { result = true; }
+            return result;           
+        }
     }
 }
--- LibraryManagement.Domain/LMDAL/UserRepository.cs
+++ LibraryManagement.Domain/LMDAL/UserRepository.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LibraryManagement.Domain.Interfaces;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
-namespace LibraryManagement.Domain.LMDAL
+    public class userRepository : IUserRepository
 {
-    class UserRepository
-    {
+        SqlConnection conn;
+        SqlCommand cmduser;
+        public userRepository()
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["conBook"].ConnectionString);
+            OpenConnection();
+        }
+        void OpenConnection()
+        {
+            if (conn.State != ConnectionState.Closed)
+                conn.Close();
+            conn.Open();
+        }
+        public string getUser(string username)
+        {
+        string passowrd="";
+        cmduser = new SqlCommand("getUser", conn);
+        cmduser.Parameters.AddWithValue("@username", username);
+        cmduser.CommandType = CommandType.StoredProcedure;
+        OpenConnection();
+        try
+        {
+            SqlDataReader dr = cmduser.ExecuteReader();
+            while (dr.Read())
+            {
+                passowrd = Convert.ToString(dr[1]);
+            }
+
+        }
+        catch (Exception)
+        {
+
+            passowrd = "";
+        }
+        conn.Close();
+        return passowrd;
+        }
     }
-}
