21f4d62d8bc529c718d7778fdd04d026bb02fc4d
janashika <57354275+janashika@users.noreply.github.com>
2020-10-21 11:35:20 +0530

    change in lending book sp

--- LibraryManagement/dbo/Stored Procedures/ProcAddBookRecords.sql
+++ LibraryManagement/dbo/Stored Procedures/ProcAddBookRecords.sql
@@ -1,9 +1,9 @@
-﻿CREATE PROCEDURE ProcAddBookRecords(@UserName nvarchar(50),@BookId nvarchar(50))
+﻿CREATE PROCEDURE ProcAddBookRecords(@UserId int,@BookId nvarchar(50))
 as 
 DECLARE @Copies int
 		SET @Copies = (select Copies from Books where Id = @BookId)
 BEGIN
-	Insert into BookRecords values (@UserName,@BookId,SYSDATETIME ( ),DATEADD(day, 15, SYSDATETIME ( )),1)
+	Insert into userBooks values (@UserId,@BookId,SYSDATETIME ( ),DATEADD(day, 15, SYSDATETIME ( )),1)
 
 	Update Books set Copies = @Copies - 1 where Id = @BookId
 END
