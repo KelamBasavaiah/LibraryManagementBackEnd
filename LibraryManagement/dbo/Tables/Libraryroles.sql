0bc2476ef3a9f03b36774f1774e8269a80227bb4
SMarimuthu13 <57354218+SMarimuthu13@users.noreply.github.com>
2020-10-22 14:07:05 +0530

    Database changes in tables

---
+++ LibraryManagement/dbo/Tables/Libraryroles.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Libraryroles] (
+    [Id]       INT           NOT NULL,
+    [RoleName] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ LibraryManagement/dbo/Tables/UserDetails.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[UserDetails] (
+    [Id]      INT           IDENTITY (1, 1) NOT NULL,
+    [UserId]  INT           NOT NULL,
+    [PhoneNo] BIGINT        NULL,
+    [MailId]  NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[libraryusers] ([userId])
+);
+
