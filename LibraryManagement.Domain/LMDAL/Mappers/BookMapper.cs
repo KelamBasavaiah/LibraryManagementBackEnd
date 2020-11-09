e5fd0a34d1c5b16108b2259b67fa3835f022fbfe
basivii <kelam.basavaiah@kanini.com>
2020-11-09 16:54:15 +0530

    Base repo implementation for Books

--- LibraryManagement.Domain/LMDAL/Mappers/BookMapper.cs
+++ LibraryManagement.Domain/LMDAL/Mappers/BookMapper.cs
@@ -10,31 +10,48 @@ namespace LibraryManagement.Domain.LMDAL.Mappers
 {
     public class BookMapper
     {
-        public async Task<List<Book>> MapBooks(SqlDataReader drbook)
+       public static async Task<List<Book>> MapBooks(SqlDataReader drbook)
         {
             List<Book> books = new List<Book>();
 
             Book book = null;
-            await Task.Run(() =>
+
+            while (await drbook.ReadAsync())
             {
-                while (drbook.Read())
-                {
-                    book = new Book();
-                    book.id = drbook[0].ToString();
-                    book.name = drbook[1].ToString();
-                    book.author_name = drbook[2].ToString();
-                    book.price = Convert.ToInt32(drbook[3]);
-                    book.contact = Convert.ToInt64(drbook[4].ToString());
-                    book.edition = Convert.ToInt32(drbook[5].ToString());
-                    book.publishedDate = Convert.ToDateTime(drbook[6].ToString());
-                    book.publisher = drbook[7].ToString();
-                    book.copies = Convert.ToInt32(drbook[8].ToString());
-                    book.genres = drbook[9].ToString();
-                    books.Add(book);
-                }
-            });
+                book = new Book();
+                book.id = drbook[0].ToString();
+                book.name = drbook[1].ToString();
+                book.author_name = drbook[2].ToString();
+                book.price = Convert.ToInt32(drbook[3]);
+                book.contact = Convert.ToInt64(drbook[4].ToString());
+                book.edition = Convert.ToInt32(drbook[5].ToString());
+                book.publishedDate = Convert.ToDateTime(drbook[6].ToString());
+                book.publisher = drbook[7].ToString();
+                book.copies = Convert.ToInt32(drbook[8].ToString());
+                book.genres = drbook[9].ToString();
+                books.Add(book);
+            }
+
             return books;
 
+        }
+        public static async Task<Book> MapBook(SqlDataReader sqlData)
+        {
+            Book book = new Book();
+                while (await sqlData.ReadAsync())
+                {
+                    book.id = sqlData[0].ToString();
+                    book.name = sqlData[1].ToString();
+                    book.author_name = sqlData[2].ToString();
+                    book.price = Convert.ToInt32(sqlData[3]);
+                    book.contact = Convert.ToInt64(sqlData[4].ToString());
+                    book.edition = Convert.ToInt32(sqlData[5].ToString());
+                    book.publishedDate = Convert.ToDateTime(sqlData[6].ToString());
+                    book.publisher = sqlData[7].ToString();
+                    book.copies = Convert.ToInt32(sqlData[8].ToString());
+                    book.genres = sqlData[9].ToString();
+                }                    
+            return book;
         }
     }
 }
